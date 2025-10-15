772080b6c8d9b0bb11ee99a9348459dbcc18c920
cakironline <ckrstyles@gmail.com>
2025-10-15 21:59:46 +0300

    Update ty-107703.py

--- ty-107703.py
+++ ty-107703.py
@@ -70,7 +70,7 @@ if "data" in st.session_state:
     now = datetime.now()
 
     def durum_hesapla(row):
-        kalan_saat = (row["Kargoya Verilmesi Gereken Tarih"] - now).total_seconds() / 3600
+        kalan_saat = (row["Kargoya Verilmesi Gereken Tarih"] - (now - timedelta(hours=3))).total_seconds() / 3600
 
         if kalan_saat < 0:  # Gecikmede
             toplam_saat = -kalan_saat
