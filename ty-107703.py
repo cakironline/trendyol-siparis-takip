98698f6f6fbe8b251d367bc9b8afc7bbdaecac5c
cakironline <ckrstyles@gmail.com>
2025-10-25 13:44:52 +0300

    Update updated_at__start date in get_warehouse_code

--- ty-107703.py
+++ ty-107703.py
@@ -86,7 +86,7 @@ def get_warehouse_code(tracker_code):
     """Tek bir tracker_code için Hamurlabs API'den warehouse_code çeker."""
     payload = {
         "company_id": "1",
-        "updated_at__start": "2025-10-24 00:00:00",
+        "updated_at__start": "2025-10-10 00:00:00",
         "updated_at__end": "2025-10-25 23:22:40",
         "size": 100,
         "start": 0,
